93d9520aed29f7ac7eaa622656c18b9a09ab4bf6
Guillermo Puente <guillermo.ps09@gmail.com>
2025-03-13 14:48:03 -0300

    fix: sign operations

--- editors/drive-explorer/editor.tsx
+++ editors/drive-explorer/editor.tsx
@@ -1,6 +1,7 @@
 import { EditorProps } from "document-model";
 import { EditorLayout } from "./components/editor-layout";
 import { DocumentDriveDocument } from "document-drive";
+import { WagmiContext } from "@powerhousedao/design-system";
 
 export type IProps = EditorProps<DocumentDriveDocument>;
 
@@ -10,36 +11,38 @@ export default function Editor(props: IProps) {
       className="atlas-drive-explorer"
       style={{ padding: "0.75rem 0.75rem 0 0.75rem", boxSizing: "content-box" }}
     >
-      <EditorLayout
-        context={props.context}
-        driveId={props.document.state.global.id}
-      >
-        <style>
-          {`
-            .atlas-drive-explorer-header {
-              margin-bottom: 1em;
-            }
-            .atlas-drive-explorer > main {
-              border: 1px solid #EEEEEE;
-            }
-            
-            .atlas-drive-explorer > main > aside {
-              height: calc(100svh - 2.25rem - 18px);
-            }
-            .atlas-drive-explorer > main > div {
-              height: calc(100svh - 2.25rem - 18px);
-              overflow-y: auto;
-            }
-            .d-none {
-              display: none;
-            }
-            #document-editor-context > div.flex:first-child {
-              position: absolute;
-              right: 0;
-              top: 16px;
-            }`}
-        </style>
-      </EditorLayout>
+      <WagmiContext>
+        <EditorLayout
+          context={props.context}
+          driveId={props.document.state.global.id}
+        >
+          <style>
+            {`
+              .atlas-drive-explorer-header {
+                margin-bottom: 1em;
+              }
+              .atlas-drive-explorer > main {
+                border: 1px solid #EEEEEE;
+              }
+              
+              .atlas-drive-explorer > main > aside {
+                height: calc(100svh - 2.25rem - 18px);
+              }
+              .atlas-drive-explorer > main > div {
+                height: calc(100svh - 2.25rem - 18px);
+                overflow-y: auto;
+              }
+              .d-none {
+                display: none;
+              }
+              #document-editor-context > div.flex:first-child {
+                position: absolute;
+                right: 0;
+                top: 16px;
+              }`}
+          </style>
+        </EditorLayout>
+      </WagmiContext>
     </div>
   );
 }

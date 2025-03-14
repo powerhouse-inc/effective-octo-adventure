199829761c947894eea071606a23adf18b307b88
ryanwolhuter <dev@ryanwolhuter.com>
2025-03-14 10:50:12 +0200

    feat: update ts config and remove vite
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,30 @@
+// @ts-check
 import { default as eslint } from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
+  globalIgnores([
+    "dist/",
+    "build/",
+    "coverage/",
+    "eslint.config.js",
+  ]),
   eslint.configs.recommended,
-
-  reactPlugin.configs.flat.all,
-
-  reactPlugin.configs.flat["jsx-runtime"],
   ...tseslint.configs.stylisticTypeChecked,
   ...tseslint.configs.strictTypeChecked,
   eslintPluginPrettierRecommended,
   {
-    ignores: [
-      "node_modules/",
-      "dist/",
-      "build/",
-      "storybook-static/",
-      "coverage/",
-      "scripts/",
-    ],
-  },
-  {
-    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
       parserOptions: {
         projectService: true,
@@ -37,58 +34,18 @@ export default tseslint.config(
         },
       },
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
     rules: {
-      "react/destructuring-assignment": "off",
-      "react/function-component-definition": "off",
-      "react/prop-types": "off",
-      "react/no-unused-prop-types": "warn",
-      "react/jsx-max-depth": "warn",
-      "react/no-array-index-key": "warn",
-      "react/jsx-filename-extension": [
-        "error",
-        { extensions: [".tsx", ".ts"] },
-      ],
-      "react/jsx-props-no-spreading": [
-        "warn",
-        {
-          html: "ignore",
-        },
-      ],
-      "react/jsx-no-bind": "warn",
-      "react/require-default-props": "off",
-      "react/jsx-no-literals": "off",
-      "react/forbid-component-props": "off",
-      "react/no-multi-comp": "off",
-      "react/button-has-type": "warn",
-      "react/hook-use-state": "warn",
-      "react/jsx-no-useless-fragment": "warn",
-      "@typescript-eslint/require-await": "warn",
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "@typescript-eslint/unbound-method": "warn",
-      "@typescript-eslint/no-empty-object-type": "warn",
-      "@typescript-eslint/no-non-null-assertion": "warn",
-      "@typescript-eslint/prefer-find": "warn",
-      "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/use-unknown-in-catch-callback-variable": "warn",
-      "@typescript-eslint/restrict-plus-operands": "warn",
-      "@typescript-eslint/restrict-template-expressions": [
-        "error",
-        {
-          allowNumber: true,
-        },
-      ],
-      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/consistent-indexed-object-style": "off",
       "@typescript-eslint/no-duplicate-type-constituents": "off",
       "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/consistent-type-imports": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/prefer-function-type": "off",
       "@typescript-eslint/strict-boolean-expressions": "off",
@@ -97,21 +54,60 @@ export default tseslint.config(
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/prefer-reduce-type-parameter": "off",
+      "@typescript-eslint/prefer-for-of": "off",
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "off",
+      "@typescript-eslint/no-unnecessary-type-parameters": "off",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/prefer-find": "warn",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/use-unknown-in-catch-callback-variable": "warn",
+      "@typescript-eslint/restrict-plus-operands": "warn",
+      "@typescript-eslint/return-await": "warn",
+      "@typescript-eslint/no-dynamic-delete": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-unnecessary-condition": "warn",
-      "@typescript-eslint/no-unnecessary-type-parameters": "warn",
+      "@typescript-eslint/restrict-template-expressions": [
+        "warn",
+        {
+          allowNumber: true,
+        },
+      ],
     },
   },
   {
-    files: ["**/*.stories.tsx", "**/*.test.ts", "**/*.test.tsx"],
-    rules: {
-      "react/jsx-props-no-spreading": [
-        "warn",
-        {
-          html: "ignore",
-        },
-      ],
-      "react/jsx-no-bind": "warn",
-      "react/button-has-type": "warn",
+    files: ["**/*.tsx"],
+    ...reactPlugin.configs.flat.all,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
-  },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: {
+      "react/require-default-props": "off",
+      "react/jsx-no-literals": "off",
+      "react/forbid-component-props": "off",
+      "react/no-multi-comp": "off",
+      "react/destructuring-assignment": "off",
+      "react/function-component-definition": "off",
+      "react/prop-types": "off",
+      "react/jsx-max-depth": "off",
+      "react/button-has-type": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/no-unused-prop-types": "warn",
+      "react/no-array-index-key": "warn",
+      "react/jsx-no-bind": "warn",
+      "react/hook-use-state": "warn",
+      "react/jsx-no-useless-fragment": "warn",
+    },
+  }
 );

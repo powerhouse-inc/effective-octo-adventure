8ed2fa8ead3a306b21127d56ae47d47c37c3413b
acaldas <acaldas.dev@gmail.com>
2025-04-10 16:38:41 +0100

    release: 1.2.19 - updated design system dev

--- editors/drive-explorer/components/driver-layout.tsx
+++ editors/drive-explorer/components/driver-layout.tsx
@@ -1,8 +1,8 @@
 import {
   Icon,
   FileItem,
+  type BaseUiFileNode,
   ToastContainer,
-  UiFileNode,
 } from "@powerhousedao/design-system";
 import {
   Sidebar,
@@ -164,23 +164,32 @@ export function DriverLayout({
                             uiNode={{
                               kind: "FILE",
                               id,
-                              name: driveNodes.find((node) => node.id === id)
-                                ?.name || "",
+                              name:
+                                driveNodes.find((node) => node.id === id)
+                                  ?.name || "",
                               documentType: issue.documentType,
                               parentFolder: "",
                               driveId,
                               syncStatus: undefined,
-                              synchronizationUnits: [],
-                              sharingType: "PUBLIC"
                             }}
                             onSelectNode={(node) => setActiveNodeId(node.id)}
-                            isAllowedToCreateDocuments={false} onRenameNode={function (name: string, uiNode: UiFileNode): void {
+                            isAllowedToCreateDocuments={false}
+                            onRenameNode={function (
+                              name: string,
+                              uiNode: BaseUiFileNode,
+                            ): void {
                               throw new Error("Function not implemented.");
-                            } } onDuplicateNode={function (uiNode: UiFileNode): void {
+                            }}
+                            onDuplicateNode={function (
+                              uiNode: BaseUiFileNode,
+                            ): void {
                               throw new Error("Function not implemented.");
-                            } } onDeleteNode={function (uiNode: UiFileNode): void {
+                            }}
+                            onDeleteNode={function (
+                              uiNode: BaseUiFileNode,
+                            ): void {
                               throw new Error("Function not implemented.");
-                            } }                           
+                            }}
                           />
                         ))}
                       </div>

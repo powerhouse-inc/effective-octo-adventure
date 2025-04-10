ab91be25e48aa8577f8e6bd7bc6422f15e31956f
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2025-04-10 17:18:43 -0300

    fix(atlas): add new fuctions and comment others until the data its correct

--- document-models/atlas-scope/src/reducers/context.ts
+++ document-models/atlas-scope/src/reducers/context.ts
@@ -7,7 +7,7 @@
 import { type AtlasScopeContextOperations } from "../../gen/context/operations.js";
 
 export const reducer: AtlasScopeContextOperations = {
-  // TODO:Disable the save in the history
+  // TODO: Implement this in  here and schema
   // addContextDataOperation(state, action, dispatch) {
   //   state.originalContextData = state.originalContextData.filter(
   //     (ocd) => ocd.id !== action.input.id,
@@ -24,19 +24,30 @@ export const reducer: AtlasScopeContextOperations = {
   //     (ocd) => ocd.id !== action.input.id,
   //   );
   // },
-  addContextDataOperation(state, action, dispatch) {
+  // addContextDataOperation(state, action, dispatch) {
   
-    state.originalContextData.push({
+  //   state.originalContextData.push({
+  //     id: action.input.id,
+  //     name: action.input.name || null,
+  //     docNo: action.input.docNo || null,
+  //   });
+  // },
+
+  // removeContextDataOperation(state, action, dispatch) {
+  //   state.originalContextData = []
+  // },
+  addContextDataOperation(state, action, dispatch) {
+    const newContextData = {
       id: action.input.id,
       name: action.input.name || null,
       docNo: action.input.docNo || null,
-    });
+    };
+    state.originalContextData=[newContextData,...state.originalContextData];
   },
 
   removeContextDataOperation(state, action, dispatch) {
     state.originalContextData = []
   },
-
   setProvenanceOperation(state, action, dispatch) {
     state.provenance = action.input.provenance || null;
   },

e0a95de5b77e38d56f1725a580c23bd3c3e7c58a
Frank <frank.pfeift@protonmail.com>
2025-04-11 08:52:57 +0200

    fix: reactor and connect start issue

--- document-models/index.ts
+++ document-models/index.ts
@@ -4,5 +4,3 @@ export { module as AtlasExploratory } from "./atlas-exploratory/index.js";
 export { module as AtlasFoundation } from "./atlas-foundation/index.js";
 export { module as AtlasGrounding } from "./atlas-grounding/index.js";
 export { module as AtlasFeedbackIssues } from "./atlas-feedback-issues/index.js";
-
-export * from "./atlas-feedback-issues/index.js";

55b09d9c1d857f397962fb516fe1c20006c5230e
nesdc88 <nestor.diaz@dspot.com.pl>
2025-05-24 16:07:10 +0200

    feat: get the timestamp corresponding to the base document

--- editors/atlas-foundation-editor/components/FoundationForm.tsx
+++ editors/atlas-foundation-editor/components/FoundationForm.tsx
@@ -2,6 +2,7 @@ import { cn, type ViewMode } from "@powerhousedao/document-engineering/scalars";
 import ContentCard from "../../shared/components/content-card.js";
 import {
   fetchSelectedPHIDOption,
+  getBaseDocumentTimestamp,
   getCardVariant,
   getStringValue,
   getTagText,
@@ -71,6 +72,8 @@ export function FoundationForm({
       (documentState.atlasType as ParsedNotionDocumentType) || "article",
     ),
   );
+
+  console.log("getBaseDocumentTimestamp", getBaseDocumentTimestamp(document));
 
   return (
     <FormModeProvider mode={mode}>

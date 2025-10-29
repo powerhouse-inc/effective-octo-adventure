e025e5cc66571bbb54180c68b779944db764b652
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-10-29 12:58:22 -0500

    fixing some codegen issues

--- document-models/generate-all-doc-models.sh
+++ document-models/generate-all-doc-models.sh
@@ -4,5 +4,6 @@ pushd ./document-models/atlas-scope && rm -rf gen && pnpm generate --file atlas-
 pushd ./document-models/atlas-exploratory && rm -rf gen && pnpm generate --file atlas-exploratory.json && popd
 pushd ./document-models/atlas-foundation && rm -rf gen && pnpm generate --file atlas-foundation.json && popd
 pushd ./document-models/atlas-grounding && rm -rf gen && pnpm generate --file atlas-grounding.json && popd
-pushd ./document-models/atlas-multiparent && rm -rf gen && pnpm generate --file atlas-multiparent.json && popd
+pushd ./document-models/atlas-multi-parent && rm -rf gen && pnpm generate --file atlas-multi-parent.json && popd
 pushd ./document-models/atlas-set && rm -rf gen && pnpm generate --file atlas-set.json && popd
+pushd ./document-models/atlas-feedback-issues && rm -rf gen && pnpm generate --file atlas-feedback-issues.json && popd

c68727bef263d60f9500461cc2dd4b51a9af284a
Benjamin Jordan (@thegoldenmule) <benjamin@thegoldenmule.com>
2025-10-29 10:37:56 -0500

    about to throw a shoe

--- document-models/generate-all-doc-models.sh
+++ document-models/generate-all-doc-models.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-pushd ./document-models/atlas-scope && ph generate --file atlas-scope.json && popd
-pushd ./document-models/atlas-exploratory && ph generate --file atlas-exploratory.json && popd
-pushd ./document-models/atlas-foundation && ph generate --file atlas-foundation.json && popd
-pushd ./document-models/atlas-grounding && ph generate --file atlas-grounding.json && popd
-pushd ./document-models/atlas-multiparent && ph generate --file atlas-multiparent.json && popd
-pushd ./document-models/atlas-set && ph generate --file atlas-set.json && popd
+pushd ./document-models/atlas-scope && pnpm generate --file atlas-scope.json && popd
+pushd ./document-models/atlas-exploratory && pnpm generate --file atlas-exploratory.json && popd
+pushd ./document-models/atlas-foundation && pnpm generate --file atlas-foundation.json && popd
+pushd ./document-models/atlas-grounding && pnpm generate --file atlas-grounding.json && popd
+pushd ./document-models/atlas-multiparent && pnpm generate --file atlas-multiparent.json && popd
+pushd ./document-models/atlas-set && pnpm generate --file atlas-set.json && popd
